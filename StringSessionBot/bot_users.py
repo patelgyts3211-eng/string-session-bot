44edcec1654bde7f4bebe6a9c29960739b9f263b
patelgyts3211-eng <patelgyts3211@gmail.com>
2026-07-08 08:15:39 +0530

    Update bot_users.py

--- StringSessionBot/bot_users.py
+++ StringSessionBot/bot_users.py
@@ -1,21 +1,27 @@
-from pyrogram.types import Message
 from pyrogram import Client, filters
+from pyrogram.types import Message
 from StringSessionBot.database import SESSION
 from StringSessionBot.database.users_sql import Users, num_users
 
 
 @Client.on_message(~filters.service, group=1)
 async def users_sql(_, msg: Message):
-    if msg.from_user:
-        q = SESSION.query(Users).get(int(msg.from_user.id))
-        if not q:
+    if not msg.from_user:
+        return
+
+    try:
+        user = SESSION.get(Users, int(msg.from_user.id))
+
+        if user is None:
             SESSION.add(Users(msg.from_user.id))
             SESSION.commit()
-        else:
-            SESSION.close()
+
+    except Exception as e:
+        SESSION.rollback()
+        print("Database Error:", e)
 
 
-@Client.on_message(filters.user(6174058850) & filters.command("stats"))
+@Client.on_message(filters.user(7383472822) & filters.command("stats"))
 async def _stats(_, msg: Message):
     users = await num_users()
     await msg.reply(f"ᴛᴏᴛᴀʟ ᴜsᴇʀs : {users}", quote=True)
